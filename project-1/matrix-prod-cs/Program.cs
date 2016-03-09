4411a006ff21c414e2ba138874f474fc8aa36618
ferrolho <henriqueferrolho@gmail.com>
2016-03-09 15:05:09 +0000

    C# almost done: missing OnMult and OnMultLine

--- project-1/matrix-prod-cs/Program.cs
+++ project-1/matrix-prod-cs/Program.cs
@@ -4,6 +4,26 @@ namespace matrix_prod_cs
 {
     public class Program
     {
+        public static void OnMult(int m_ar, int m_br)
+        {
+
+        }
+
+        public static void OnMultLine(int m_ar, int m_br)
+        {
+
+        }
+
+        public static float produtoInterno(float[] v1, float[] v2, int col)
+        {
+            float sum = 0;
+
+            for (uint i = 0; i < col; i++)
+                sum += v1[i] * v2[i];
+
+            return sum;
+        }
+
         public static void Main(string[] args)
         {
             int op = 1;
@@ -18,21 +38,33 @@ namespace matrix_prod_cs
                 Console.WriteLine("0. Exit");
                 Console.WriteLine("-------------------------");
 
-                Console.Write("Selection?: ");
-                String input = Console.ReadLine();
+                Console.Write("Selection: ? ");
+                string input = Console.ReadLine();
 
                 if (int.TryParse(input, out op))
                 {
-                    switch (op)
+                    if (op == 1 || op == 2)
                     {
-                        case 1:
-                            break;
+                        Console.Write("Dimensions: lins cols ? ");
+                        string[] tokens = Console.ReadLine().Split();
 
-                        case 2:
-                            break;
+                        int lin, col;
+                        if (tokens.Length == 2 && int.TryParse(tokens[0], out lin) && int.TryParse(tokens[1], out col))
+                        {
+                            switch (op)
+                            {
+                                case 1:
+                                    OnMult(lin, col);
+                                    break;
 
-                        default:
-                            break;
+                                case 2:
+                                    OnMultLine(lin, col);
+                                    break;
+
+                                default:
+                                    break;
+                            }
+                        }
                     }
                 }
                 else
@@ -42,6 +74,7 @@ namespace matrix_prod_cs
                 }
 
                 Console.WriteLine();
+
             } while (op != 0);
         }
     }
