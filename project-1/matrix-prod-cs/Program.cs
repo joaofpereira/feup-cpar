6235911cf7c6721efdce8c078f92da0420e1c269
ferrolho <henriqueferrolho@gmail.com>
2016-03-09 12:12:08 +0000

    created C# in VScode

---
+++ project-1/matrix-prod-cs/Program.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace matrix_prod_cs
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World 1");
+            Console.WriteLine("Hello World 2");
+            Console.WriteLine("Hello World 3");
+            Console.Read();
+        }
+    }
+}
